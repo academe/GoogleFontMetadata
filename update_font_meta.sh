2a1362ef6cbe8b4c3796f392c3e7641c48268d82
Jason Judge <jason.judge@consil.co.uk>
2013-09-20 22:31:27 +0100

    Create update_font_meta.sh

---
+++ update_font_meta.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This, and the scripts it calls, are quick and dirty, and I
+# make no appologies for that.
+# This needs to be run from the parent directory of the 
+# webfonts hg repository, extracted to googlefonts-hg
+# It is all quick and dirty and I make no apologies.
+
+# Update the hg respository.
+
+cd googlefonts-hg
+hg pull
+hg update
+cd -
+
+# Combine the metadata files.
+
+./combine_meta.sh >font_meta.json
+
+# Extract the font groups data.
+
+./group_categories.sh >font_categories.json
+
